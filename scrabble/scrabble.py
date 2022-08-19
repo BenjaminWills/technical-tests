baa4126997e041857e23b1a515ec92a7c3a85a11
BenjaminWills <benjaminwills047@gmail.com>
2022-08-19 14:23:04 +0100

    added bag function to initialise the bag

--- scrabble/scrabble.py
+++ scrabble/scrabble.py
@@ -1,15 +1,19 @@
 import random
+import string
+ALPHABET = [*string.ascii_uppercase]
+
 def get_chars_of_word(word):
     return [*word]
 
 def get_letter_score(letter):
-    score_dict = {'E':1,'A':1,'I':1,'O':1,'N':1,'R':1,'T':1,'L':1,'S':1,'U':1,
-                  'D':2,'G':2,
-                  'B':3,'C':3,'M':3,'P':3,
-                  'F':4,'H':4,'V':4,'W':4,'Y':4,
-                  'K':5,
-                  'J':8,'X':8,
-                  'Q':10,'Z':10}
+    score_dict = {
+        'E':1,'A':1,'I':1,'O':1,'N':1,'R':1,'T':1,'L':1,'S':1,'U':1,
+        'D':2,'G':2,
+        'B':3,'C':3,'M':3,'P':3,
+        'F':4,'H':4,'V':4,'W':4,'Y':4,
+        'K':5,
+        'J':8,'X':8,
+        'Q':10,'Z':10}
     return score_dict[letter]
 
 def get_word_score(word):
@@ -19,8 +23,23 @@ def get_word_score(word):
         word_score += get_letter_score(char.upper())
     return word_score
 
+def get_bag():
+    bag = {
+        'E':12,
+        'A':9,'I':9,
+        '0':8,
+        'N':6,'R':6,'T':6,
+        'L':4,'S':4,'U':4,'D':4,
+        'G':3,
+        'B':2,'C':2,'M':2,'P':2,'F':2,'H':2,'V':2,'W':2,'Y':2,
+        'K':1,'J':1,'X':1,'Q':1,'Z':1
+    }
+    return bag
+
 def get_tiles(num = 7):
-    
-
-
+    hand = []
+    for i in range(num):
+        random_index = random.randint(0,25)
+        hand.append(ALPHABET[random_index])
+    return hand
 
